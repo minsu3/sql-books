b184a79a79834e486904d5d983a0e2c7a38bea9f
Min Su Kim <kim97minsu@gmail.com>
2019-11-24 15:22:42 -0800

    added author and categories routes

--- index.js
+++ index.js
@@ -3,100 +3,149 @@ let express = require('express');
 let database = require('./database.js')
 let app = express();
 
-
 // Middleware
 app.use(express.json());
 
-
 // Configuration Variables
 const port = 3000;
-
 
 // Routes
 app.get('/', (request, response) => {
   response.send('Visit /api/books to see our list of titles');
 });
 
+
+
 ////////////////////
 //  BOOK ROUTES  
 ///////////////////
 
-
-
-
 // get all books
-app.get('/api/books',  (req, res) => {
+app.get('/api/books',  (request, response) => {
   // send all books as JSON response
   const getAllBooks = 'SELECT * FROM books';
 
   database.all(getAllBooks, (error, results) => {
     if(error) {
-      console.log("Get all books table failed", error)
-      res.sendStatus(500);
+      console.log("Get all books table failed", error);
+      response.sendStatus(500); //Internal Server Error
     }
     else {
-      res.status(200).json(results);
+      response.status(200).json(results);
     }
   });
 });
 
-
-
-
 // get one book
-app.get('/api/books/:id',  (req, res) => {
+app.get('/api/books/:id',  (request, response) => {
   // find one book by its id
-  const getOneBook = 'SELECT * FROM books WHERE '
+  const bookId = request.params.id;
+  const getOneBook = `SELECT * FROM books WHERE books.oid = ?`;
+
+  database.all(getOneBook, [bookId], (error, results) => {
+    if(error) {
+      console.log("Get one book from table failed", error);
+      response.sendStatus(500);
+    }
+    else {
+      response.status(200).json(results);
+    }
+  });
 });
-
-
-
-
 
 // create new book
-app.post('/api/books',  (req, res) => {
+app.post('/api/books',  (request, response) => {
   // create new book with form data (`req.body`)
+  const reqBody = [request.body.title, request.body.author_id, request.body.image, request.body.release_date, request.body.page_count]
+  const createNewBook = 'INSERT INTO books VALUES = (?, ?, ?, ?, ?)';
 
+  database.run(createNewBook, [request.body.title], error => {
+    if(error) {
+      console.log(`Create new book with title: ${request.body.title} failed`);
+      response.sendStatus(500);
+    }
+    else {
+      console.log(`Added new book ${request.body.title}`);
+      response.status(200).json(results);
+    }
+  });
 });
 
-
-
-
 // update book
-app.put('/api/books/:id', (req,res) => {
+app.put('/api/books/:id', (request,response) => {
   // get book id from url params (`req.params`)
-  const bookId = req.params.id;
+  const bookId = request.params.id;
   const updateOneBook = `UPDATE books SET TITLE = ? WHERE books.oid = ${bookId}`;
 
   // use the query string and req.body to run the query in the database
-  database.run(updateOneBook, [req.body.title], error => {
+  database.run(updateOneBook, [request.body.title], error => {
     if(error) {
       console.log(`Update book with ID ${bookId} failed.`, error);
-      res.sendStatus(500);
+      response.sendStatus(500);
     }
     else {
-      console.log(`Book with ID ${bookId} was updated successfully`)
-      res.sendStatus(200);
+      console.log(`Book with ID ${bookId} was updated successfully`);
+      response.sendStatus(200);
+    }
+  });
+});
+
+// delete book
+app.delete('/api/books/:id', (request, response) => {
+  // get book id from url params (`req.params`)
+  const bookId = [request.params.id];
+  const deleteBooks = `DELETE FROM books WHERE ? = oid`;
+
+  database.run(deleteBooks, bookId, error => {
+    if(error) {
+      console.log(`Delete from book with ID of ${bookId} failed`);
+      response.sendStatus(500);
+    }
+    else{
+      console.log(`Delete from book with ID of ${bookId} succeeded`);
+      response.sendStatus(200);
     }
   });
 });
 
 
-
-// delete book
-app.delete('/api/books/:id', (req, res) => {
-  // get book id from url params (`req.params`)
-
-});
 
 ////////////////////////
 // TODO: AUTHOR ROUTES
 ////////////////////////
 
 // 1. Write a route to retrieve all authors from the database
+app.get('/api/authors', (request,response) => {
+  const getAllAuthors = 'SELECT * FROM authors';
+
+  database.all(getAllAuthors, (error, results) => {
+    if(error) {
+      console.log("Retrieving all authors failed");
+      response.sendStatus(500);
+    }
+    else {
+      console.log("Retrieving all authors succeeded");
+      response.status(200).json(results);
+    }
+  })
+})
 
 // 2. Write a route to add a new author to the database
+app.post('/api/authors', (request, response) => {
+  reqBody = [request.body.name];
+  const addNewAuthor = 'INSERT INTO authors VALUES (?)';
 
+  database.all(addNewAuthor, reqBody, error => {
+    if(error) {
+      console.log(`Adding new author ${request.body.name} to the database failed`);
+      response.sendStatus(500);
+    }
+    else {
+      console.log(`Adding new author ${request.body.name} to the database succeeded`);
+      response.sendStatus(200);
+    }
+  });
+});
 // 3. (OPTIONAL) Write the rest of the RESTful routes for this entity for extra practice.
 
 
@@ -106,9 +155,35 @@ app.delete('/api/books/:id', (req, res) => {
 //////////////////////////
 
 // 1. Add a route to retrieve all categories from the database
+app.get('/api/categories', (request, response) => {
+  const getAllCategories = "SELECT * FROM categories";
 
+  database.all(getAllCategories, (error, results) => {
+    if(error) {
+      console.log("Get all categories from the database failed");
+      response.sendStatus(500);
+    }
+    else {
+      response.status(200).json(results);
+    }
+  });
+});
 // 2. Write a route to add a new category to the database
+app.post('/api/categories', (request, response) => {
+  reqBody = [request.body.name];
+  const addNewCategory = 'INSERT INTO categories VALUES (?)';
 
+  database.all(addNewCategory, reqBody, error => {
+    if(error) {
+      console.log(`Adding a new category ${request.body.name} to the database failed`);
+      response.sendStatus(500);
+    }
+    else {
+      console.log(`Adding a new category ${request.body.name} to the database succeeded`);
+      response.sendStatus(200);
+    }
+  })
+})
 // 3. (OPTIONAL) Write the rest of the RESTful routes for this entity for extra practice.
 
 
@@ -118,34 +193,35 @@ app.delete('/api/books/:id', (req, res) => {
 /////////////////////////////////////////////////
 
 // Retrieve a book's categories using book ID 
-app.get('/api/books/:id/categories',(req, res) => {
-  const bookId = req.params.id;
+app.get('/api/books/:id/categories',(request, response) => {
+  const bookId = request.params.id;
   const queryString = "SELECT * FROM books_categories WHERE book_id = ?";
 
   database.all(queryString, [bookId], (error, results) => {
     if (error) {
       console.log(error);
-      res.sendStatus(500) //Internal Server Error
+      response.sendStatus(500); 
     }
     else {
-      res.status(200).json(results);
+      response.status(200).json(results);
     }
   })
 });
 
 // Create an association between a book and a category using the book ID 
-app.post('/api/books/:id/categories', (req, res) => {
-  const bookId = req.params.id;
-  const catId = req.body.category_id
+app.post('/api/books/:id/categories', (request, response) => {
+  const bookId = request.params.id;
+  const catId = request.body.category_id
   const insertString = "INSERT INTO books_categories VALUES (?, ?)"
 
   database.run(insertString, [bookId, catId], error => {
     if (error) {
-      console.log(error);
-      res.sendStatus(500);
+      console.log(`Error in creating association between book and category`);
+      response.sendStatus(500);
     }
     else {
-      res.sendStatus(200)
+      console.log(`Creating association between book and category succeeded`);
+      response.sendStatus(200);
     }
   })
 });
